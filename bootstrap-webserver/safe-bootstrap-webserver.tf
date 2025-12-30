b4663cab2d41d63d00b7e2ad7d98642ce3830bf3
Petsku01 <79454252+Petsku01@users.noreply.github.com>
2025-12-30 12:24:41 +0200

    Update AWS provider version to 6.0

--- bootstrap-webserver/safe-bootstrap-webserver.tf
+++ bootstrap-webserver/safe-bootstrap-webserver.tf
@@ -12,7 +12,7 @@
 terraform {
   required_version = ">= 1.6"
   required_providers {
-    aws = { source = "hashicorp/aws", version = "~> 5.70" }
+    aws = { source = "hashicorp/aws", version = "~> 6.0" }
     http = { source = "hashicorp/http", version = "~> 3.4" }
     random = { source = "hashicorp/random", version = "~> 3.6" }
   }
